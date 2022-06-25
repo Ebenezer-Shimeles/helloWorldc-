c329d91cee85135b20d0b2fe75b73708e698b481
Ebenezer <ebenezertesfaye@yahoo.com>
2022-06-26 02:03:52 +0600

    Added checks

--- CSHARPPROJECT2/Program.cs
+++ CSHARPPROJECT2/Program.cs
@@ -2,7 +2,21 @@
 class Program { 
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Please choose an Operation: * + / ** -");    
+        Console.WriteLine("Hello, Please choose an Operation: * + /  -");
+        char op = Console.ReadLine()[0];
+
+        if(op != '*' && op != '+' && op != '/' && op != '-')
+        {
+            Console.Error.WriteLine("Error invalid operation\nTry Again");
+            Program.Main(null);
+        }
+        double op1, op2;
+
+        Console.WriteLine("\nPlease Input operand One:");
+        Console.WriteLine("\nPlease Input operand Two:");
+
+        switch (op) { }
+
     }
 
 }
